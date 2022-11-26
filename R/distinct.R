fb981a5790b4b8358cf5dd005bb41f1e798bc97a
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 02:14:32 +0900

    Bug fix for distinct

--- R/distinct.R
+++ R/distinct.R
@@ -15,7 +15,8 @@ distinct.sticky_rowwise_df <- function(.data, ..., .keep_all = FALSE) {
 }
 
 distinct_sticky <- function(data_distinct, data, ...) {
-  data_summarised <- summarise(group_by(data, ...))
+  data_summarised <- summarise(group_by(data, ...),
+                               .groups = "drop")
   data_distinct <- dplyr::bind_cols(data_distinct, data_summarised[!names(data_summarised) %in% names(data_distinct)])
   restore_sticky_attrs(data_distinct, data)
 }
---
+++ tests/testthat/test-distinct.R
@@ -0,0 +1,10 @@
+test_that("distinct", {
+  data <- new_sticky_tibble(list(col_1 = letters,
+                                 col_2 = letters,
+                                 col_3 = letters),
+                            cols = col_3,
+                            class = "my_tbl_df")
+
+  expect_no_error(data |>
+                    distinct(col_1, col_3))
+})
